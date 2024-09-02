61add0dd471e524ea08d5594dc31cd22f88d53e8
InnCent <innokentiykim90@gmail.com>
2024-09-02 22:37:58 +0500

    HW_2 script for csv-file added

--- 2.1_Databases/work_with_database/phones/models.py
+++ 2.1_Databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+    price = models.FloatField()
+    image = models.URLField(max_length=200)
+    release_date  = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f"{self.name}: {self.price}"
