5aaeffa40759e629507be8fe55f30abffe854e07
InnCent <innokentiykim90@gmail.com>
2024-09-03 21:55:13 +0500

    HW_3 task_2 db_filled

--- 2.1_Databases/models_list_displaying/books/views.py
+++ 2.1_Databases/models_list_displaying/books/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
+    books_list = [book for book in Book.objects.all()]
     context = {}
     return render(request, template, context)
--- 2.1_Databases/models_list_displaying/main/urls.py
+++ 2.1_Databases/models_list_displaying/main/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-from books.views import books_view
+from django.contrib import admin
+from django.urls import path, include
+
+from books import views
 
 urlpatterns = [
-    path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('', include('books.urls')),
 ]
