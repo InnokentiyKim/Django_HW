d274a53099558468b2519ce95d09748a00cde315
InnCent <innokentiykim90@gmail.com>
2024-09-07 00:26:08 +0500

    HW_4 orm_migrations models view admin_models added

--- 2.2_Databases_2/orm_migrations/school/admin.py
+++ 2.2_Databases_2/orm_migrations/school/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
 
-from .models import Student, Teacher
+from .models import Student, Teacher, Schedule
+
+
+class ScheduleInline(admin.TabularInline):
+    model = Schedule
+    extra = 1
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'group')
     list_filter = ('group', )
+    inlines = [ScheduleInline, ]
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('name', 'subject')
     list_filter = ('subject', )
+    inlines = [ScheduleInline, ]
