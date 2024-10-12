71c3c7c26664c4cc1f0a42b2d7076eba8d5f09c3
InnCent <innokentiykim90@gmail.com>
2024-10-12 22:08:22 +0500

    HW_3_4 some tests added

--- 3.4_Django_Testing/django_testing/django_testing/settings.py
+++ 3.4_Django_Testing/django_testing/django_testing/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_django_testing',
+        'USER': os.getenv('USER', 'postgres'),
+        'PASSWORD': os.getenv('PASSWORD', 'postgres'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- 3.4_Django_Testing/django_testing/tests/students/test_courses_api.py
+++ 3.4_Django_Testing/django_testing/tests/students/test_courses_api.py
@@ -1,8 +1,40 @@
+import pytest
 from tests.conftest import courses_factory, students_factory, client
 
+BASE_COURSE_URL = "/api/v1/courses/"
+BASE_STUDENT_URL = "/api/v1/students/"
 
-def test_retrieve_course(client, students_factory, courses_factory):
-    base_url = "/api/v1/courses/"
+@pytest.mark.django_db
+def test_retrieve_course(client, courses_factory):
     course = courses_factory()
+    response = client.get(f"{BASE_COURSE_URL}{course.id}/")
+    data = response.json()
 
+    assert response.status_code == 200
+    assert data['name'] == course.name
+
+
+@pytest.mark.django_db
+def test_list_courses(client, courses_factory):
+    courses = courses_factory(_quantity=10)
+    response = client.get(BASE_COURSE_URL)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == len(courses)
+    for i, course in enumerate(courses):
+        assert data[i]['name'] == course.name
+
+
+@pytest.mark.django_db
+def test_filter_course_by_id(client, courses_factory):
+    courses = courses_factory(_quantity=10)
+    course_index = 3
+    course_id = courses[course_index].id
+    response = client.get(f"{BASE_COURSE_URL}?id={course_id}")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 1
+    assert data[0]['name'] == courses[course_index].name
 
